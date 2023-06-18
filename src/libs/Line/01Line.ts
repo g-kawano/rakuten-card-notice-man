e7f8719648c1b56f6dd5d6a243933171f5d6e99f
g-kawano <go.departure@gmail.com>
2023-06-18 18:39:04 +0900

    Line クラスのテストコードを追加した。

--- src/libs/Line/01Line.ts
+++ src/libs/Line/01Line.ts
@@ -1,13 +1,12 @@
 import { Setting } from "@/00Setting";
 import { Client, Message } from "@line/bot-sdk";
 
-const lineSettings = new Setting();
-
 /**
  * LINE 操作用クラス
  */
 export class Line {
   line: Client;
+  setting: Setting;
   /**
    * LINE にメッセージをプッシュする
    * push は無料プランでは 200 通までなので使いすぎ注意
@@ -17,13 +16,15 @@ export class Line {
     // @ts-ignore ts(2304) GAS のライブラリから読み込んでいるため
     //https://github.com/kobanyan/line-bot-sdk-gas
     lineClient: Client = new LineBotSDK.Client({
-      channelAccessToken: lineSettings.LINE_CHANNEL_ACCESS_TOKEN,
-    })
+      channelAccessToken: new Setting().LINE_CHANNEL_ACCESS_TOKEN,
+    }),
+    setting: Setting = new Setting()
   ) {
     this.line = lineClient;
+    this.setting = setting;
   }
 
   pushMessage(message: Message | Message[]): void {
-    this.line.pushMessage(lineSettings.LINE_GROUP_ID, message);
+    this.line.pushMessage(this.setting.LINE_GROUP_ID, message);
   }
 }
---
+++ tests/Line/Line.test.ts
@@ -0,0 +1,35 @@
+import { Client, Message } from "@line/bot-sdk";
+import { Setting } from "@/00Setting";
+import { Line } from "@/libs/Line/01Line";
+
+jest.mock("@line/bot-sdk");
+jest.mock("@/00Setting");
+
+describe("Line class tests", () => {
+  let mockPushMessage: jest.Mock;
+  let mockSettingInstance: jest.Mocked<Setting>;
+  let mockLineInstance: Line;
+
+  beforeEach(() => {
+    mockPushMessage = jest.fn();
+    (Client as jest.MockedClass<typeof Client>).mockImplementation(() => {
+      return {
+        pushMessage: mockPushMessage,
+      } as unknown as Client; // Client に対する型アサーション
+    });
+
+    mockSettingInstance = new Setting() as jest.Mocked<Setting>;
+    mockSettingInstance.LINE_CHANNEL_ACCESS_TOKEN = "test_token";
+    mockSettingInstance.LINE_GROUP_ID = "test_group";
+
+    mockLineInstance = new Line(new Client({ channelAccessToken: "test_token" }), mockSettingInstance);
+  });
+
+  it("should correctly push a message", () => {
+    const testMessage: Message = { type: "text", text: "Hello, world" };
+
+    mockLineInstance.pushMessage(testMessage);
+
+    expect(mockPushMessage).toHaveBeenCalledWith(mockSettingInstance.LINE_GROUP_ID, testMessage);
+  });
+});
